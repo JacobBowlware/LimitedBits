4c8c3a16979d0f1c6193fec56acca4402a1c0f9b
Jacob Bowlware <jacobbowlware@gmail.com>
2023-07-14 21:03:10 -0500

    Implement Joi input validation for login form.

---
+++ client/limited-bits/src/components/WebJoi.js
@@ -0,0 +1,61 @@
+import Joi from 'joi-browser';
+
+const schema = {
+    email: Joi.string().required().email().label('Email'),
+    password: Joi.string().required().min(8).label('Password'),
+    confirmPassword: Joi.ref('password'),
+};
+
+/**
+ * Validate the input property
+ * @param {*} param0 The input property
+ * @param {*} password The password property (optional - used for confirm password validation)
+ * @returns 
+ */
+const validateProperty = (name, value, password = "") => {
+    console.log(name, value);
+    const obj = { [name]: value };
+    const currentSchema = { [name]: schema[name] };
+    const { error } = Joi.validate(obj, currentSchema);
+
+    if (name === 'confirmPassword') {
+        if (password === value) return null;
+        else {
+            return 'Both passwords must match!';
+        }
+    }
+    return error ? error.details[0].message : null;
+}
+
+/**
+ * Validate input change event
+ * @param {*} Input The input property
+ * @param {*} setErrors The setErrors state property
+ * @param {*} isLogin Is the current page a login page (optional - used for confirm password validation)
+ * @param {*} password The password property (optional - used for confirm password validation)
+ * @param {*} confirmPassword The confirmPassword property (optional - used for confirm password validation)
+ * @returns 
+ */
+const validateChange = (Input, setErrors, isLogin = false, password = "", confirmPassword = "") => {
+    if (isLogin) {
+        setErrors(prevState => ({ ...prevState, [Input.name]: validateProperty(Input, password) }));
+        return;
+    }
+
+    if (Input.name === 'confirmPassword') {
+        if (password === Input.value)
+            setErrors(prevState => ({ ...prevState, confirmPassword: "" }));
+        else
+            setErrors(prevState => ({ ...prevState, confirmPassword: "Both passwords must match!" }));
+    }
+    else if ((Input.name === 'password')) {
+        if (Input.value === confirmPassword)
+            setErrors(prevState => ({ ...prevState, confirmPassword: "" }));
+        else
+            setErrors(prevState => ({ ...prevState, confirmPassword: "Both passwords must match!" }));
+    }
+
+    setErrors(prevState => ({ ...prevState, [Input.name]: validateProperty(Input, password) }));
+}
+
+export { validateChange, validateProperty };
--- client/limited-bits/src/pages/Login.tsx
+++ client/limited-bits/src/pages/Login.tsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+// Joi validation
+import { validateProperty } from "../components/WebJoi";
+
+
 const Login = () => {
     const [email, setEmail] = useState<String>("");
     const [password, setPassword] = useState<String>("");
+
+    const [emailError, setEmailError] = useState<String>("");
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
         console.log("Login form submitted");
+    }
+
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const error = validateProperty("email", e.target.value);
+        setEmailError(error);
     }
 
     return (
@@ -19,17 +30,22 @@ const Login = () => {
                         Login to  LimitedBits
                     </h1>
                     <form className="form login-card__form" onSubmit={(e) => handleSubmit(e)}>
+                        {emailError && <p className="form-error">{emailError}</p>}
                         <input type="email" id="email" className="form-input"
-                            placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                            placeholder="Email" onChange={(e) => {
+                                setEmail(e.target.value);
+                                handleEmailChange(e);
+                            }} />
                         <input type="password" id="password" className="form-input"
                             placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-                        <button type="submit" className="btn form-btn">Login</button>
+                        <button type="submit" className="btn form-btn"
+                            disabled={emailError ? true : false}>Login</button>
                     </form>
                     <p className="form-helper">
                         Don't have an account? <Link className="form-link" to="/sign-up">Sign up</Link>
                     </p>
                 </div>
-            </div>
+            </div >
         </>
     );
 }
